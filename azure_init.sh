90d0d8b96f01248300ba8ef3e74fa22fc37ed032
Hossam <hossam.darwish@gmail.com>
2018-01-13 01:06:42 +0200

    Update azure_init.sh

--- azure_init.sh
+++ azure_init.sh
@@ -6,8 +6,8 @@ cd xmr-stak
 sudo echo -e "\n* soft memlock 262144 \n* hard memlock 262144 \n" >>  /etc/security/limits.conf
 sudo echo -e "\nvm.nr_hugepages=128\n" >>  /etc/sysctl.conf
 sudo sysctl -p
-cmake ./
-make
+cmake ./ -DCUDA_ENABLE=OFF
+make 
 make install
 echo "Log off from all user ttys"
 echo "after that run ~/xmr-stak/bin/xmr-stak -c ~/xmr-stak/bin/config.txt"
