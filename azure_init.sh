d0244642037e0509d94f11ec099084be60c9b542
Hossam <hossam.darwish@gmail.com>
2018-01-13 00:34:11 +0200

    Create azure_init.sh

---
+++ azure_init.sh
@@ -0,0 +1,13 @@
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt install -y libmicrohttpd-dev  libhwloc-dev  libssl-dev build-essential git cmake
+git clone https://github.com/hossamdarwish/xmr-stak-cpu
+cd xmr-stak-cpu
+
+sudo echo -e "\n* soft memlock 262144 \n* hard memlock 262144 \n" >>  /etc/security/limits.conf
+sudo echo -e "\nvm.nr_hugepages=128\n" >>  /etc/sysctl.conf
+sudo sysctl -p
+cmake ./
+make
+make install
+echo "Log off from all user ttys"
+echo "after that run ~/xmr-stak-cpu/bin/xmr-stak-cpu -c ~/xmr-stak-cpu/bin/config.txt"
