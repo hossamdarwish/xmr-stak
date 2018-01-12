64f2d9935f6b8ae09173717c0ebeb9eca45e4479
Hossam <hossam.darwish@gmail.com>
2018-01-13 00:19:21 +0200

    Update donate-level.hpp

--- xmrstak/donate-level.hpp
+++ xmrstak/donate-level.hpp
@@ -1,4 +1,4 @@
-#pragma once
+0#pragma once
 
 /*
  * Dev donation.
@@ -15,4 +15,4 @@
  *
  */
 
-constexpr double fDevDonationLevel = 2.0 / 100.0;
+constexpr double fDevDonationLevel = 0.0 / 100.0;
