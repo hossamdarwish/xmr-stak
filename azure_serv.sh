f056dbf5e3f091227f7465e754be76fcf7905a1f
Hossam <hossam.darwish@gmail.com>
2018-01-13 03:34:22 +0200

    Update azure_serv.sh

--- azure_serv.sh
+++ azure_serv.sh
@@ -1,13 +1,14 @@
-sudo cat <<EOT >> /lib/systemd/system/xmr.service
+sudo tee /lib/systemd/system/xmr.service <<END
 [Unit]
 Description=xmr
 After=network.target
 [Service]
-ExecStart=/home/sala/xmr-stak/bin/xmr-stak 
+ExecStart=/home/sala/xmr-stak/bin/xmr-stak -c /home/sala/xmr-stak/bin/config.txt --cpu /home/sala/xmr-stak/bin/cpu.txt
 User=root
 [Install]
 WantedBy=multi-user.target
-EOT
 
 sudo systemctl daemon-reload
 sudo systemctl enable xmr.service
+sudo systemctl start xmr.service
+END
